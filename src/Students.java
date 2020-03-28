2b6dcf730c257b94d4b54059ece29cf6313e6796
ssverud <svirpstef@gmail.com>
2020-03-28 13:01:34 +0100

    Code clean up. Comments added

--- src/Students.java
+++ src/Students.java
@@ -46,45 +46,6 @@ public class Students {
                 if(rs2.next())
                     System.out.println("Average grade for course " + courseID + " = " + rs2.getFloat(1));
 
-
-/*
-                // The same with a prepared statement
-                String psql= "SELECT * FROM Departure WHERE stationname= ?";
-                PreparedStatement pstmt = null;
-                pstmt = conn.prepareStatement(psql);
-                pstmt.setString(1, "København H");
-                rs = pstmt.executeQuery();
-                PresentDepartures(rs);
-/*
-                // Do the join from the yesterdays exercise
-                String ptripsql= "SELECT D1.stationname as startstation,D1.time as starttime, " +
-                        "D2.stationname as endstation, D2.time as endtime" +
-                        "  FROM Departure as D1 " +
-                        "join Departure as D2 on D1.trainid =D2.trainid " +
-                        "WHERE D1.stationname= ? and D2.stationname= ? and D1.time<D2.time and D1.time >= ?";
-                PreparedStatement pTripStmt = conn.prepareStatement(ptripsql);
-                System.out.println("Which station do you wish to find departures for?");
-                departureStation = scanner.nextLine();
-                System.out.println("Which station do you want to go to?");
-                String endStation = scanner.nextLine();
-                System.out.println("Which time do you depart (as float)?");
-                float  ftime = scanner.nextFloat();
-                pTripStmt.setString(1,departureStation);
-                pTripStmt.setString(2,endStation);
-                pTripStmt.setFloat(3, ftime);
-                rs = pTripStmt.executeQuery();
-                PresentTrips(rs);
-
-                // Insert and Update
-                String sqlInsert = "INSERT INTO departure (stationname, trainid, time) " +
-                        " VALUES ('København H',2,5.00),('Høje Tåstrup',2,5.15)," +
-                        "('Roskilde',2,5.22),('Ringsted',2,5.38),('Odense',2,6.36)";
-                PreparedStatement pstmtInsert = conn.prepareStatement(sqlInsert);
-                int rowsaffected=pstmtInsert.executeUpdate();
-                System.out.println("Number of rows affected "+ rowsaffected);
-
-*/
-
         } catch (SQLException e) {
             e.printStackTrace();
         } finally {
@@ -95,18 +56,6 @@ public class Students {
                     e.printStackTrace();
                 }
             }
-        }
-    }
-
-    // Print function from ResultSet
-    static public void PresentStudents(ResultSet res)
-            throws SQLException {
-        if (res == null)
-            System.out.println("No records for customer");
-        while (res != null & res.next()) {
-            String fname = res.getString("FirstName");
-            String lname = res.getString("LastName");
-            System.out.println(lname + " " + fname);
         }
     }
 
@@ -121,11 +70,6 @@ public class Students {
             System.out.println(studentID + " " + grade);
         }
     }
-
-
-
-
-
 }
 
 
